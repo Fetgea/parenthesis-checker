e69f14b79de21777c193f618bf168948b7bba4a7
Dinar <dinar@reaspekt.com>
2021-04-29 10:43:20 +0300

    Changed checkParenthesis call

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include __DIR__ . "/functions.php";
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $postBody = file_get_contents('php://input');
     try {
-        if (checkParenthesis($postBody, 1)) {
+        if (checkParenthesis($postBody)) {
             http_response_code(200);
             echo "Ok";
         }
